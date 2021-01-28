19177b155fd5da02f457a01de808d653ac2b5e81
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2021-01-28 11:36:30 -0300

    Installs shoulda matcher and adapt country test

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,4 @@
 class Country < ApplicationRecord
-  validates :name, presence: true, uniqueness: true 
+  validates :name, presence: true, uniqueness: true
+  has_many :states
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper' 
 
 describe Country do 
+  # build from factory bot
   it "its valid with a name" do 
-    country = Country.new(name: 'Brasil') 
+    country = build(:country)
     expect(country).to be_valid 
   end
 
-  it "its invalid without a name" do 
-    country = Country.new() 
-    expect(country).not_to be_valid 
-  end
+  # shoulda matcher sintax
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to have_many(:states) }
 
+  # build and create from factory bot
   it "its invalid with an used name" do 
-    country = Country.create(name: 'Brasil') 
-    country = Country.new(name: 'Brasil') 
+    country = create(:country, name: 'Brasil')
+    country = build(:country, name: 'Brasil')
     expect(country).not_to be_valid 
   end
 end
